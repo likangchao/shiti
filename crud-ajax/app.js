b71615037ba481e25a56539000a552553be0da36
李康超 <2513125485@qq.com>
2019-08-20 19:48:09 +0800

    完成

---
+++ crud-ajax/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const employeeRouter = require('./routes/employee')
+const cors=require('cors')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const app = express()
+app.use(cors())
+
+app.use(morgan('tiny'))
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: false
+}))
+
+app.use('/public', express.static(path.join(__dirname, './public/')))
+
+app.use(express.static(path.join(__dirname, './views/')))
+
+app.use('/employees', employeeRouter)
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
